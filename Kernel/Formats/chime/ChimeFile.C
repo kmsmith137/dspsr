a6e067e58e52507daf9d88a545df6aaf9eaae472
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-08-28 11:12:32 -0400

    encyclopedia of Input virtual functions

--- Kernel/Formats/chime/ChimeFile.C
+++ Kernel/Formats/chime/ChimeFile.C
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-inline bool endswith(const std::string &str, const std::string &suffix)
+inline bool endswith(const string &str, const string &suffix)
 {
   return std::equal(suffix.rbegin(), suffix.rend(), str.rbegin());
 }
@@ -38,12 +38,12 @@ dsp::ChimeFile::ChimeFile() : File("CHIME")
   // The "2" here means { real, complex }.
   obs->set_ndim(2);
 
-  obs->set_nchan(ch_vdif_assembler::dspsr_handle::get_nchan());
+  obs->set_nchan(ch_vdif_assembler::dspsr_handle::nfreq);
   obs->set_npol(2);
   obs->set_nbit(4);
   obs->set_telescope("CHIME");
   obs->set_centre_frequency(600.);   // MHz
-  obs->set_rate(ch_vdif_assembler::dspsr_handle::get_rate());
+  obs->set_rate(ch_vdif_assembler::dspsr_handle::sampling_rate_Hz);
 
   // I'm not sure if these values are correct!
   obs->set_bandwidth(400.);   // "negative = lsb; positive = usb"
@@ -57,7 +57,6 @@ dsp::ChimeFile::ChimeFile() : File("CHIME")
   //   - a bunch of strings which didn't seem important (receiver, identifier, etc.)
   //
 }
-
 
 
 dsp::ChimeFile::~ChimeFile() 
@@ -79,21 +78,110 @@ bool dsp::ChimeFile::is_valid(const char *filelist_filename) const
 }
 
 
-void dsp::ChimeFile::open_file(const char *filelist_filename)
+void dsp::ChimeFile::open(const char *filelist_filename)
 {
   if (assembler_handle)
-    throw runtime_error("internal error: ChimeFile:open_file() was called twice?!");
+    throw runtime_error("internal error: ChimeFile:open() was called twice?!");
 
   this->assembler_handle = ch_vdif_assembler::dspsr_handle::make(filelist_filename);
 }
 
 
-// Overrides File::set_total_samples().  We have no way of determining the sample count
-// in advance, so we just don't initialize it.  I think this is OK, since class DADABuffer
-// does the same thing.
+void dsp::ChimeFile::close()
+{
+  throw runtime_error("ChimeFile: File::close() called");
+}
+
+
+bool dsp::ChimeFile::eod()
+{
+  throw runtime_error("ChimeFile: Input::eod() called");
+}
+
+
+void dsp::ChimeFile::load_data(BitSeries *data)
+{
+  throw runtime_error("ChimeFile: Input::load_data() called");
+}
+
+
+void dsp::ChimeFile::seek(int64_t offset, int whence)
+{
+  throw runtime_error("ChimeFile: Input::seek() called");
+}
+
+
+// -------------------------------------------------------------------------------------------------
+
+
+void dsp::ChimeFile::copy(const Input *input)
+{
+  throw runtime_error("ChimeFile: Input::copy() called");
+}
+
+void dsp::ChimeFile::seek(MJD mjd)
+{
+  throw runtime_error("ChimeFile: Input::seek() called");
+}
+
+void dsp::ChimeFile::rewind()
+{
+  throw runtime_error("ChimeFile: Seekable::rewind() called");
+}
+
+void dsp::ChimeFile::set_eod(bool eod)
+{
+  throw runtime_error("ChimeFile: Seekable::set_eod() called");
+}
+
+int64_t dsp::ChimeFile::load_bytes(unsigned char* buffer, uint64_t bytes)
+{
+  throw runtime_error("ChimeFile: Seekable::load_bytes() called");
+}
+
+int64_t dsp::ChimeFile::seek_bytes(uint64_t bytes)
+{
+  throw runtime_error("ChimeFile: Seekable::seek_bytes() called");
+}
+
+uint64_t dsp::ChimeFile::recycle_data(BitSeries* data)
+{
+  throw runtime_error("ChimeFile: Seekable::recycle_data() called");
+}
+
+uint64_t dsp::ChimeFile::get_current_sample()
+{
+  throw runtime_error("ChimeFile: Seekable::get_current_sample() called");
+}
+
+void dsp::ChimeFile::reopen()
+{
+  throw runtime_error("ChimeFile: File::reopen() called");
+}
+
+void dsp::ChimeFile::open_file(const char *filelist_filename)
+{
+  throw runtime_error("ChimeFile: File::open_file() called");
+}
+
+int64_t dsp::ChimeFile::fstat_file_ndat(uint64_t tailer_bytes)
+{
+  throw runtime_error("ChimeFile: File::fstat_file_ndat() called");
+}
+
+int64_t dsp::ChimeFile::pad_bytes(unsigned char *buffer, int64_t bytes)
+{
+  throw runtime_error("ChimeFile: File::pad_bytes() called");
+}
+
 void dsp::ChimeFile::set_total_samples()
 {
-  return;
+  throw runtime_error("ChimeFile: File::set_total_samples() called");
+}
+
+void dsp::ChimeFile::open_fd(const string &filename)
+{
+  throw runtime_error("ChimeFile: File::open_fd() called");
 }
 
 
--- Kernel/Formats/chime/dsp/ChimeFile.h
+++ Kernel/Formats/chime/dsp/ChimeFile.h
@@ -19,11 +19,44 @@ namespace dsp {
   public:
     ChimeFile();
     virtual ~ChimeFile();
-	  
-    // All of these are overrides of virtual functions in base classes.
-    virtual bool is_valid(const char* filelist_filename) const;
-    virtual void open_file(const char* filelist_filename);
-    virtual void set_total_samples();
+
+    // File virtuals.  Note that it's slightly easier to override open() than open_file().
+    virtual bool is_valid(const char *filelist_filename) const;
+    virtual void open(const char *filelist_filename);
+    virtual void close();
+
+    // Input virtuals.
+    virtual bool eod();
+    virtual void load_data(BitSeries *data);
+    virtual void seek(int64_t offset, int whence=0);
+    
+    // Here is a list of Input virtuals which we don't override, since the default implementations seem OK.
+    //   get_output(), set_output(), has_output()
+    //   get_block_size(), set_block_size()
+    //   get_overlap(), set_overlap(), get_stride()
+    //   get_info(), get_total_samples()
+    //   operation()
+    //   mark_output()
+
+    // Input virtuals which currently throw exceptions.
+    virtual void copy(const Input *input);
+    virtual void seek(MJD mjd);
+
+    // Seekable virtuals which currently throw exceptions.
+    virtual void rewind();
+    virtual void set_eod(bool eod);
+    virtual int64_t load_bytes(unsigned char *buffer, uint64_t bytes);
+    virtual int64_t seek_bytes(uint64_t bytes);
+    virtual uint64_t recycle_data(BitSeries *data);
+    virtual uint64_t get_current_sample();
+    
+    // File virtuals which currently throw exceptions.
+    virtual void reopen();
+    virtual void open_file(const char *filelist_filename);
+    virtual int64_t fstat_file_ndat(uint64_t tailer_bytes);
+    virtual int64_t pad_bytes(unsigned char *buffer, int64_t bytes);
+    virtual void set_total_samples();   // note that get_total_samples() is a member of Input, but set_total_samples() is a member of File
+    virtual void open_fd(const std::string &filename);
 
   protected:
     ch_vdif_assembler::dspsr_handle *assembler_handle;
