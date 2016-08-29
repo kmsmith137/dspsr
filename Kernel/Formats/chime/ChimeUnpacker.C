64ba8a866a6a53ddbf9b77db53447c3aa4568928
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-08-29 12:32:18 -0400

    ChimeUnpacker body

--- Kernel/Formats/chime/ChimeUnpacker.C
+++ Kernel/Formats/chime/ChimeUnpacker.C
@@ -21,7 +21,54 @@ bool dsp::ChimeUnpacker::matches(const Observation *observation)
 
 void dsp::ChimeUnpacker::unpack()
 {
-  throw runtime_error("ChimeUnpacker::unpack() called");
+  // Sanity checks on input
+
+  if (input->get_nchan() != (unsigned)ch_vdif_assembler::dspsr_handle::nfreq)
+    throw runtime_error("ChimeUnpacker: unexpected value of input->nchan");
+  if (input->get_npol() != 2)
+    throw runtime_error("ChimeUnpacker: unexpected value of input->npol");
+  if (input->get_ndim() != 2)
+    throw runtime_error("ChimeUnpacker: unexpected value of input->ndim");
+  if (input->get_ndat() != (unsigned)ch_vdif_assembler::dspsr_handle::nt_chunk)
+    throw runtime_error("ChimeUnpacker: unexpected value of input->ndat");
+  if (input->get_nbit() != 4)
+    throw runtime_error("ChimeUnpacker: unexpected value of input->nbit");
+
+  // Sanity checks on output
+
+  if (output->get_order() != TimeSeries::OrderFPT)
+    throw runtime_error("ChimeUnpacker: unexpected value of output->order");
+  if (output->get_nchan() != (unsigned)ch_vdif_assembler::dspsr_handle::nfreq)
+    throw runtime_error("ChimeUnpacker: unexpected value of output->nchan");
+  if (output->get_npol() != 2)
+    throw runtime_error("ChimeUnpacker: unexpected value of output->npol");
+  if (output->get_ndim() != 2)
+    throw runtime_error("ChimeUnpacker: unexpected value of output->ndim");
+  if (output->get_ndat() != (unsigned)ch_vdif_assembler::dspsr_handle::nt_chunk)
+    throw runtime_error("ChimeUnpacker: unexpected value of output->ndat");
+
+  // Unpack logic.  Conveniently, ch_vdif_assembler keeps the data in the same
+  // (freq, pol, time, dim) order as dspsr.
+
+  for (int ifreq = 0; ifreq < ch_vdif_assembler::dspsr_handle::nfreq; ifreq++) {
+    for (int ipol = 0; ipol < 2; ipol++) {
+      float *dst = output->get_datptr(ifreq, ipol);
+      const uint8_t *src = input->get_rawptr() + (2*ifreq+ipol) * ch_vdif_assembler::dspsr_handle::nt_chunk;
+
+      // FIXME should write assembly language kernel in ch_vdif_assembler_kernels.hpp
+      for (int i = 0; i < ch_vdif_assembler::dspsr_handle::nt_chunk; i++) {
+	if (src[i] == 0) {
+	  // invalid entry
+	  dst[2*i] = dst[2*i+1] = 0.0;
+	}
+	else {
+	  // offset decode
+	  dst[2*i] = (float)((src[i] & 0xf0) >> 4) - 8.0;
+	  dst[2*i+1] = (float)(src[i] & 0x0f) - 8.0;
+	}
+      }
+    }
+  }
 }
 
 
